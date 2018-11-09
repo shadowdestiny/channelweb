23c9a25dc8241a1d8f5d5ad9149c5c13a4e02c4e
Luis Marin <lrm.programador@gmail.com>
2018-11-09 12:23:11 -0400

    Euromillion adaptacion

--- src/component/global/header/Logo.js
+++ src/component/global/header/Logo.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 //---
-import logo from '../../image/rimac-logo.png';
 //---
 import '../../css/global/Logo.css';
 import {Link} from 'react-router-dom';
@@ -13,7 +12,7 @@ class Logo extends Component {
         <nav className='navbar navbar-default navbar-static-top' role='navigation' style={{marginBottom: 0}}>
             <div className='navbar-header'>
                 <div className='Logo'>
-                     <a href='Home.js'> <img src={logo} alt='logo'/> </a>
+                     <a href='Home.js'>  </a>
                </div>
             </div>
         </nav>
